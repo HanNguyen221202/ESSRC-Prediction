6c3871f9c929128148c2b41ad59a4f71d270370d
HanNguyen221202 <163711137+HanNguyen221202@users.noreply.github.com>
2026-06-04 11:01:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown(
     <style>
     /* ---- THÊM ĐOẠN NÀY ĐỂ ĐỔI MÀU NỀN TOÀN TRANG ---- */
     .stApp {
-        background-color: #BEBDB8 !important; /* Màu xám bạc nhạt */
+        background-color: #EBEAE5 !important; /* Màu xám bạc nhạt */
     }
     /* 1. Kích thước chữ của các mục lớn (st.subheader) */
     h3 {
